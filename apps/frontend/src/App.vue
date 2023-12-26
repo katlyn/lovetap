70f9d08344ee025c391803c09a628621c431c0a0
Katlyn Lorimer <katlyn@katlyn.dev>
2023-12-26 13:08:22 -0700

    chore(frontend): update eslint config

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router"
+
+import HelloWorld from "./components/HelloWorld.vue"
 </script>
 
 <template>
--- apps/frontend/src/components/TheWelcome.vue
+++ apps/frontend/src/components/TheWelcome.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+import CommunityIcon from "./icons/IconCommunity.vue"
+import DocumentationIcon from "./icons/IconDocumentation.vue"
+import EcosystemIcon from "./icons/IconEcosystem.vue"
+import SupportIcon from "./icons/IconSupport.vue"
+import ToolingIcon from "./icons/IconTooling.vue"
+import WelcomeItem from "./WelcomeItem.vue"
 </script>
 
 <template>
@@ -33,7 +33,7 @@ import SupportIcon from './icons/IconSupport.vue'
     you need to test your components and web pages, check out
     <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
     <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
+    >Cypress Component Testing</a
     >.
 
     <br />
@@ -67,7 +67,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
     Discord server, or
     <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
+    >StackOverflow</a
     >. You should also subscribe to
     <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
     the official
