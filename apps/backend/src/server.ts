2098a537f5f095dd02fde518313d1f806802bf8e
Katlyn Lorimer <katlyn@katlyn.dev>
2024-01-20 18:42:26 -0900

    feat(backend): serve frontend static files

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,6 +5,10 @@ import customTransformKeyword from "ajv-custom-transform"
 import traps from "@dnlup/fastify-traps"
 import env from "./config/env.js";
 import routes from "./routes/index.js";
+import fastifyStatic from "@fastify/static";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import {readFileSync} from "fs";
 
 interface ServerOptions {}
 
@@ -24,6 +28,16 @@ export default async function buildServer (opts: ServerOptions = {}) {
   await server.register(fastifyCors, {
     origin: env.http.corsUrl,
     credentials: true
+  })
+
+  const __dirname = dirname(fileURLToPath(import.meta.url))
+  const frontendRoot = join(__dirname, "../../frontend/dist")
+  await server.register(fastifyStatic, {
+    root: frontendRoot
+  })
+
+  server.setNotFoundHandler(async (request, reply) => {
+    return reply.sendFile("index.html")
   })
 
   await server.register(routes)
