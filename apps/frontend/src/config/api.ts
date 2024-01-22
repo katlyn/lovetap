2ee38a6d2b53529bff5948ec8683923ef7263637
Katlyn Lorimer <katlyn@katlyn.dev>
2024-01-21 23:16:15 -0900

    fix(frontend): hopefully last url fix

--- apps/frontend/src/config/api.ts
+++ apps/frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 import LovetapApi from "api-wrapper"
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL.length > 0 ? import.meta.env.VITE_API_BASE_URL : window.location.origin + "/api"
+const API_BASE = import.meta.env.VITE_API_BASE_URL.length > 0 ? import.meta.env.VITE_API_BASE_URL : window.location.origin + "/api/"
 
 export default new LovetapApi(API_BASE)
